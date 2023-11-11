16a7209b9cc68b2e593b7c6a12956520a6bba04f
erni94 <ernest.nigmatullin@gmail.com>
2023-11-11 23:35:03 +0500

    init

---
+++ src/components/film-card/film-card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css.';
+
+function FilmCard() {
+    return (
+        <div className="film-cards">
+            <FilmCard />
+        </div>
+    );
+}
+
+export default FilmCard;
