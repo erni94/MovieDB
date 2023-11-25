d1197124be73adf9c70259cddcb49971133582fa
erni94 <ernest.nigmatullin@gmail.com>
2023-11-25 17:09:21 +0500

    разделил App для Tabs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,96 +1,39 @@
 import React from 'react';
 import './style.css';
-import APIClient from "../../services/api-client";
-import FilmCard from "../film-card";
-import { Spin, Alert } from 'antd';
-import SearchBar from "../search-bar";
-import CustomPagination from "../pagination";
+import RatedFilms from "../rated-films/rated-films";
+import FilmsList from "../films-list/films-list";
+import { Tabs } from 'antd';
+
 
 class App extends React.Component {
 
-
-        apiClient = new APIClient();
-
-        state = {
-            filmObject: [],
-            loading: true,
-            error: null,
-            currentPage: 1,
-            totalPage: 0,
-            searchTerm: '',
-            isMounted: false,
-        };
-
-
-
-        componentDidMount() {
-            if(!(localStorage.getItem('guestSessionId'))) {
-                this.apiClient.createGuestSession();
-            }
-            this.setState({ isMounted: true }, () => {
-                this.getFilms('avengers', this.state.currentPage);
-            });
-        }
-
-    getFilms = (text, page) => {
-
-        if (!navigator.onLine) {
-            this.setState({ error: 'No internet connection.' });
-            return;
-        }
-        this.setState({ loading: true });
-        this.apiClient.searchMovies(text, page)
-            .then(({ results, total_pages }) => {
-                console.log(total_pages)
-                this.setState({ filmObject: results, loading: false, totalPage: total_pages, searchTerm: text });
-            })
-            .catch(() => {
-                this.setState({ error: 'Failed to load films.', loading: false });
-            });
+    state={
+        activeTab: 'Search '
     }
 
-    onChangePage = (page) => {
-        this.setState({ currentPage: page });
-        this.getFilms(this.state.searchTerm, page);
+    items = [
+        {
+            key: 'Search',
+            label: 'Search',
+            children: <FilmsList />
+        },
+        {
+            key: 'Rated',
+            label: 'Rated',
+            children: <RatedFilms />,
+        }
+    ]
+
+    onChange = (key) => {
+        this.setState({ activeTab: key });
     }
-
-
 
 
     render() {
-        const { filmObject, loading, error, currentPage, totalPage } = this.state;
+        return (
+            <Tabs defaultActiveKey="Search" items={this.items} onChange={this.onChange} />
+        )
 
-
-        const renderFilmListOrSpinner = () => {
-            if (loading) {
-                return <Spin size="large" />;
-            } else {
-                if (filmObject.length === 0) {
-                    return <h2>No films found.</h2>;
-                } else {
-                    return filmObject.map((film) => (
-                        <FilmCard key={film.id} filmObject={film} />
-                    ));
-                }
-            }
-        };
-
-        if (error) {
-            return <Alert message="Error" description={error} type="error" />;
-        }
-
-        return (<>
-            <SearchBar getFilms={this.getFilms}/>
-            <div className="film-cards">
-                {renderFilmListOrSpinner()}
-            </div>
-                <CustomPagination
-                    currentPage={currentPage}
-                    totalPage={totalPage}
-                    onChangePage={this.onChangePage}
-                />
-            </>
-        );
     }
 }
 
